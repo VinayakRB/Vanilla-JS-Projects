e60673d1ba05dadc6ad2920128590f008fc765a5
VinayakRB <vnyk.baghel@gmail.com>
2020-11-17 20:50:50 +0530

    get the weather data and update the ui with fetched data

--- Weather App js/scripts/app.js
+++ Weather App js/scripts/app.js
@@ -1,6 +1,26 @@
 const cityForm = document.querySelector(`.change-location`);
 const card = document.querySelector(`.card`);
 
+const updateUI = data => {
+
+    // destructure properties
+    const { cityDet, weather } = data;
+
+    //update details template
+    details.innerHTML = `
+                <h5 class="my-3">${cityDet.EnglishName}</h5>
+                <div class="my-3">${weather.WeatherText}</div>
+                <div class="display-4 my-4">
+                    <span>${weather.Temperature.Metric.Value}</span>
+                    <span>&deg;C</span>
+                </div>
+    `;
+
+    if(card.classList.contains(`d-none`)) {
+        card.classList.remove(`d-none`);
+    }
+}
+
 const updateCity = async (city) => {
     // console.log(city);
     const cityDet = await getCity(city);
