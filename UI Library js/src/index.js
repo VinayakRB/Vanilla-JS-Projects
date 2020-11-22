302364c6c6ba39b214effa405a3fefb7055f7ff9
VinayakRB <vnyk.baghel@gmail.com>
2020-11-23 02:04:35 +0530

    added snackbar ui

--- UI Library js/src/index.js
+++ UI Library js/src/index.js
@@ -1,6 +1,7 @@
 import Tooltip from './project ui/tooltip';
 import Dropdown from './project ui/dropdown';
 import Tabs from './project ui/tabs';
+import Snackbar from './project ui/snackbar';
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 tooltip.init();
@@ -13,6 +14,13 @@ dropdowns.forEach(dropdown => {
   instance.init();
 });
 
-
 const tabs = new Tabs(document.querySelector('.tabs'));
 tabs.init();
+
+const snackbar = new Snackbar();
+snackbar.init();
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+  snackbar.show('You clicked me :)');
+});
