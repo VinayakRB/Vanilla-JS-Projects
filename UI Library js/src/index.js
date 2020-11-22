f854e0fea95da1da246691c182ff29cd9a2db12d
VinayakRB <vnyk.baghel@gmail.com>
2020-11-23 01:07:46 +0530

    added tooltip ui

--- UI Library js/src/index.js
+++ UI Library js/src/index.js
@@ -1,3 +1,13 @@
-import './style.css';
+import Tooltip from './project ui/tooltip';
+import Dropdown from './project ui/dropdown';
 
-console.log(`test`);
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+tooltip.init();
+
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
