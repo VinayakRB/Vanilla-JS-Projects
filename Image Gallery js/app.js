c07c90276ff074d9df00c0c513623355e1d62513
VinayakRB <vnyk.baghel@gmail.com>
2020-11-29 20:52:04 +0530

    added class for ease of access

--- Image Gallery js/app.js
+++ Image Gallery js/app.js
@@ -1,30 +1,40 @@
 const panels = Array.from(document.querySelector('.panels').children);
-// console.log(panels);
-function toggleText(e) {
-  if(e.propertyName === 'flex-grow' && this.classList.contains('open')) {
-    this.classList.toggle('open-active');
-  } else if(e.propertyName === 'flex-grow') {
-    this.classList.toggle('open-active');
+
+class Gallery {
+  constructor() {}
+
+  togglePanel() {
+      this.classList.toggle('open');
+  }
+
+  toggleText(e) {
+    if(e.propertyName === 'flex-grow' && this.classList.contains('open')) {
+      this.classList.toggle('open-active');
+    } else if(e.propertyName === 'flex-grow' && this.classList.contains('open-active')) {
+      this.classList.toggle('open-active');
+    }
+  }
+
+  onHover() {
+    panels.forEach(panel => {
+      panel.addEventListener('mouseenter', function() {
+        this.classList.add('open');
+      });
+      panel.addEventListener('transitionend', this.toggleText);
+      panel.addEventListener('mouseleave', function() {
+        this.classList.remove('open', 'open-active');
+      });
+    });
+  }
+
+  onClick() {
+    panels.forEach(panel => {
+      panel.addEventListener('click', this.togglePanel);
+      panel.addEventListener('transitionend', this.toggleText);
+    });
   }
 }
 
-function togglePanel() {
-  this.classList.toggle('open');
-}
-
-//On Hover
-// panels.forEach(panel => {
-//   panel.addEventListener('mouseenter', function() {
-//     this.classList.add('open');
-//   });
-//   panel.addEventListener('transitionend', toggleText);
-//   panel.addEventListener('mouseleave', function() {
-//     this.classList.remove('open', 'open-active');
-//   });
-// });
-
-//On Click and transition end
-panels.forEach(panel => {
-  panel.addEventListener('click', togglePanel);
-  panel.addEventListener('transitionend', toggleText);
-})
+const obj = new Gallery();
+obj.onClick();
+// obj.onHover();
